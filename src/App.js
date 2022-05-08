44549ed3e368707b50b30bb3b576d6d1cda48ad6
SanzinaHossain <sanzina67hossain@gmail.com>
2022-05-08 10:45:30 +0600

    header done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Blogs from './Components/Blogs/Blogs';
 import About from './Components/About/About';
+import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
 function App() {
   return (
     <div>
@@ -14,6 +16,8 @@ function App() {
       <Route path="/" element={<Home></Home>}></Route>
       <Route path="/blogs" element={<Blogs></Blogs>}></Route>
       <Route path="/about" element={<About></About>}></Route>
+      <Route path="/login" element={<Login></Login>}></Route>
+      <Route path="/register" element={<Register></Register>}></Route>
       </Routes>
     </div>
   );
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import pic from "../../Images/Logo/logo.jpg";
+import pic from "../../Images/Logo/Logo.jpg";
+import './Header.css';
 const Header = () => {
   return (
     <div >
          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
   <Navbar.Brand>
-      <img src={pic} alt=""/>
+      <img className="header-img" src={pic} alt=""/>
   </Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
@@ -16,6 +17,10 @@ const Header = () => {
       <Nav.Link className='text-white-700' as={Link} to="/">Home</Nav.Link>
       <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
       <Nav.Link as={Link} to="/about">About</Nav.Link>
+    </Nav>
+    <Nav className="fixed">
+       <Nav.Link as={Link} to="/login">Sign In</Nav.Link>
+       <Nav.Link as={Link} to="/register">Register</Nav.Link>
     </Nav>
   </Navbar.Collapse>
   </Container>
